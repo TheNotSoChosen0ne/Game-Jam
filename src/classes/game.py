080456e3a54e3f251744236e476cde6b056c4c80
Loann Badina <loann.badina@epitech.eu>
2025-02-08 16:19:34 +0100

    [~] Pushing first play button functionnal version

--- src/classes/game.py
+++ src/classes/game.py
@@ -1,14 +1,21 @@
 import pygame
+from typing import List
+
+from src.classes.room import *
+from src.classes.player import *
+from src.classes.menu import *
+from src.classes.window import *
 
 DAY = 1
 NIGHT = 0
 
 class Game():
-    def __init__(self, player):
+    def __init__(self, player : Player, rooms : List["Room"], menu : Menu):
         self.actual_room = "office"
-        self.rooms = []
+        self.rooms = rooms
         self.player = player
         self.cycle = DAY
+        self.menu = menu
         self.font = pygame.font.Font("pixel_font.otf", 42)
         self.time = pygame.time.Clock()
         return
@@ -22,5 +29,9 @@ class Game():
     
     def switchRoom(self, new_room : str):
         self.actual_room = new_room
+        return
+    
+    def runGame(self, screen):
+        self.rooms[self.actual_room].draw(screen)
         return
 
