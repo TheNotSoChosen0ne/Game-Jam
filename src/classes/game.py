5712866f2b4f71c3c8f4872765032858ff01cb1c
Loann Badina <loann.badina@epitech.eu>
2025-02-08 14:11:44 +0100

    [~] Room system

--- src/classes/game.py
+++ src/classes/game.py
@@ -4,11 +4,10 @@ DAY = 1
 NIGHT = 0
 
 class Game():
-    def __init__(self, room, player, running : int):
-        self.room = room
+    def __init__(self, player):
+        self.actual_room = "office"
         self.rooms = []
         self.player = player
-        self.running = running
         self.cycle = DAY
         self.font = pygame.font.Font("pixel_font.otf", 42)
         self.time = pygame.time.Clock()
@@ -20,3 +19,8 @@ class Game():
         else:
             self.cycle = NIGHT
         return
+    
+    def switchRoom(self, new_room : str):
+        self.actual_room = new_room
+        return
+
--- src/classes/room.py
+++ src/classes/room.py
@@ -1,5 +1,22 @@
+from src.classes.sprites import *
+from src.classes.game import *
+
+class Door(StaticSprite):
+    
+    def __init__(self, image, pos_x, pos_y, next_room : str):
+        super().__init__(image, pos_x, pos_y)
+        self.next_room = next_room
+        return
+    
+    def open_door(self, game : Game):
+        game.switchRoom(self.next_room)
+        return
+
+
 class Room():
-    def __init__(self, sprite, objects)
+    
+    def __init__(self, sprite, objects, doors):
         self.sprite = sprite
         self.objects = objects
+        self.doors = doors
         return
