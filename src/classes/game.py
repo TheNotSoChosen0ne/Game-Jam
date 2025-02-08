4d88453d8578c719e4ab9ca44e74506ec7dc84ea
Liloche19 <lilian.locher@epitech.eu>
2025-02-08 12:32:50 +0100

    [+] Text class added

--- src/classes/game.py
+++ src/classes/game.py
@@ -4,12 +4,19 @@ DAY = 1
 NIGHT = 0
 
 class Game():
-    def __init__(self, room, player, running):
+    def __init__(self, room, player, running : int):
         self.room = room
         self.rooms = []
         self.player = player
-        self.running = running,
+        self.running = running
         self.cycle = DAY
         self.font = pygame.font.Font("pixel_font.otf", 42)
         self.time = pygame.time.Clock()
+        return
+
+    def switchTime(self):
+        if self.cycle == NIGHT:
+            self.cycle = DAY
+        else:
+            self.cycle = NIGHT
         return
--- src/classes/text.py
+++ src/classes/text.py
@@ -1,7 +1,14 @@
-import sprites
+import pygame
 
-class Game():
-    def __init__(self, text):
-        self.text = text
-        self.sprite = sprites.StaticSprite("../img/textbox.png", 0, 0)
+class Text():
+    def __init__(self, fontPath : str, size : int, pos : tuple):
+        self.fontPath = fontPath
+        self.size = size
+        self.pos = pos
+        return
+
+    def draw(self, text : str, screen, color : tuple):
+        self.font = pygame.font.Font(self.fontPath, self.size)
+        fontText = self.font.render(text, 1, color)
+        screen.blit(fontText, self.pos)
         return
